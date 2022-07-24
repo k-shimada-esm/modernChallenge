8780411f896908f7f28b7dc5ddcb9f952d89e9d8
k-shimada-esm <k_shimada@esm.co.jp>
2022-07-24 16:09:45 +0900

    1-1. カウントアップできるようにする の対応完了

--- webApplication/counterMain.js
+++ webApplication/counterMain.js
@@ -1,7 +1,7 @@
-let couter = 0;
-
-function doGet() {
+ function doGet() {
   let template = HtmlService.createTemplateFromFile('index');
-  template.counter = couter;
+  // 引数であるカウンターの開始はゼロ
+  template.counter = 0;
+  // HTML の表示
   return template.evaluate();
 }
